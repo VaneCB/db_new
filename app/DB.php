fae254ce8dd7467510997ecbee3c4bb25b31e391
vanesa <vcarrerabcn@gmail.com>
2023-03-02 14:33:13 +0100

    agregadas funciones

--- app/DB.php
+++ app/DB.php
@@ -7,22 +7,32 @@ class DB
     {
         try {
             $this->conexion = new PDO(DSN,USER,PASS);
-            //$this->conexion = new_sqli("127.0.0.1", USER, PASS, "dwes", 23313);
         } catch (PDOException $exception) {
             die ("No se ha podido conectar" . $exception->getMessage());
         }
 
     }
-
+    
+    private function ejecuta_consulta($sentencia,$valores){
+        $rtdo= $this->conexion->prepare($sentencia);
+        $rtdo->execute($valores);
+        return $rtdo;
+    }
+    
+    private function dime_familias(){
+        $sentencia = "select * from familias";
+        return $this->ejecuta_consulta($sentencia);
+    }
     public function valida_usuario($nombre,$pass){
-        $consulta =<<<FIN
-    select * 
-    from usuarios 
-    where nombre = '$nombre' 
-    and 
-    pass='$pass';
-FIN;
-        $rtdo= $this->conexion->query($consulta);
+        //$consulta ="select * from usuarios where nombre = ? and pass=?";
+       //Crear consulta
+        $consulta ="select * from usuarios where nombre = :nombre and pass=:pass";
+        $rtdo= $this->conexion->prepare($consulta);
+        $rtdo->bindParam(":nombre",$nombre);
+        $rtdo->bindParam(":pass",$pass);
+        //Ejecuto la sentencia
+        $rtdo->execute();
+    //Podria omitir el bindparam y hacerlo en el execute $rtdo->execute([$nombre,$pass]);
         if ($rtdo->rowCount() > 0){
             return true;
         }else {
